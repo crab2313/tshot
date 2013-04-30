86bc8cd6997c27a0a5196a30a181d76e925cfbcd
crab2313 <7723n12@sina.com>
2013-05-01 03:56:16 +0800

    fixed parseLink

--- TShot/Parse.hs
+++ TShot/Parse.hs
@@ -6,5 +6,5 @@ parseLink :: String -> String
 parseLink = parseMagnet
 
 parseMagnet :: String -> String
-parseMagnet link = (takeWhile (/= '&') . dropColon . dropColon . dropColon) link
-		where dropColon = dropWhile (/= ':')
+parseMagnet link = (dropColon . dropColon . dropColon . takeWhile (/= '&')) link
+		where dropColon = tail . dropWhile (/= ':')
