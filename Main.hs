5a9be0b7147be38953a978d06d09eb2a7174f1b8
crab2313 <7723n12@sina.com>
2013-04-30 18:35:21 +0800

    update Main.hs, now we simply print IDs.

--- Main.hs
+++ Main.hs
@@ -3,5 +3,9 @@ module Main(main) where
 import Network.HTTP
 import System.IO
 import TShot
+import TShot.Network
+import System.Environment(getArgs)
 
-main = putStrLn "hello\n"
+main = do args <- getArgs
+	  ids <- mapM getIDByHash args
+	  mapM_ (putStrLn . show) ids
