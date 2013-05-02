7a613d27715434c5420f261d659026ce6a29f817
crab2313 <7723n12@sina.com>
2013-05-02 13:53:17 +0800

    add Video Torrent Thumbnail data types.

--- TShot/Type.hs
+++ TShot/Type.hs
@@ -3,3 +3,19 @@ module TShot.Type where
 type HashCode = String
 type Link = String
 type VideoID = Int
+
+
+data Torrent = Torrent {
+      tVideo :: [Video],
+      tHash  :: HashCode
+    }
+
+data Video = Video {
+      videoID     :: VideoID,
+      videoName   :: String,
+      videoThumbs :: [Thumbnail]
+    }
+
+data Thumbnail = Thumbnail {
+      tbLinks :: [Link]
+    }
