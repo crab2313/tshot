a7e320e037cd48c60b765d73e5db2ef3ef98d216
crab2313 <7723n12@sina.com>
2013-05-06 00:16:38 +0800

    a ugly hack, now parseLink support hashcode.

--- TShot/Parse/Link.hs
+++ TShot/Parse/Link.hs
@@ -3,7 +3,8 @@ module TShot.Parse.Link where
 import Data.Char (toUpper)
 
 parseLink :: String -> String
-parseLink = map toUpper . parseMagnet
+parseLink link = if length link == 40 then map toUpper link else 
+		map toUpper $ parseMagnet link
 
 parseMagnet :: String -> String
 parseMagnet link = (dropColon . dropColon . dropColon . takeWhile (/= '&')) link
