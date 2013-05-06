1bbf9a005635d24efe5add1f2fcd70dcb44af7a4
crab2313 <7723n12@sina.com>
2013-05-06 15:11:57 +0800

    handle links more carefully

--- TShot/Parse/Link.hs
+++ TShot/Parse/Link.hs
@@ -3,9 +3,12 @@ module TShot.Parse.Link where
 import Data.Char (toUpper)
 
 parseLink :: String -> String
-parseLink link = if length link == 40 then map toUpper link else 
-		map toUpper $ parseMagnet link
-
+parseLink link = case takeWhile (== ':') link of
+                   "magnet" -> parseMagnet link
+                   _ -> if length link == 40 then map toUpper link
+                                              else error "What's the link?"
+                                                  
 parseMagnet :: String -> String
-parseMagnet link = (dropColon . dropColon . dropColon . takeWhile (/= '&')) link
-		where dropColon = tail . dropWhile (/= ':')
+parseMagnet =
+    map toUpper . dropColon . dropColon . dropColon . takeWhile (/= '&')
+        where dropColon = tail . dropWhile (/= ':')
