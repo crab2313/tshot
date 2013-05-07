3a5d2a45aa2d67cb96fc777200942c19e4e0bc22
crab2313 <7723n12@sina.com>
2013-05-07 16:03:07 +0800

    add thunder's link support

--- Main.hs
+++ Main.hs
@@ -12,4 +12,6 @@ main = do
   videos <- mapM getVideosByHash hashs
   mapM_ (mapM_ $ fetchVideo dir fname) videos
       where dir = "."
-	    fname id name n = intercalate "-" [name, show(id), show(n), ".tb"]
+	    fname id name n = intercalate "-" [eName, show(id), show(n), ".tb"]
+                where eName = map (\c -> if c == '/' then '-' else c) name
+                
--- TShot/Parse/Link.hs
+++ TShot/Parse/Link.hs
@@ -1,16 +1,23 @@
 module TShot.Parse.Link where
 
 import Data.Char (toUpper)
+import Codec.Binary.Base64.String (decode)
 
 parseLink :: String -> String
 parseLink link = case takeWhile (/= ':') link of
                    "magnet" -> parseMagnet link
                    _ -> if length link == 40 then map toUpper link
-                                              else error "What's the link?"
+                                  else error $ "What's the link? " ++ link
                                                   
 parseMagnet :: String -> String
 parseMagnet =
     map toUpper . dropColon . dropColon . dropColon . takeWhile (/= '&')
         where dropColon = tail . dropWhile (/= ':')
 
+parseThunder :: String -> String
+parseThunder link = parseBase64 $ drop 8 [c | c <- link, c /= '/']
+	where parseBase64 = parseLink . strip . decode
+	      strip str = take (len-4) $ drop 2 str 
+	      	where len = length str
+          
 -- TODO: parseThunder parseEd2k parseFlashGet
