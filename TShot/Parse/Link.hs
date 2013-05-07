971af48d2283e163c28a71272e6de8790cf01003
crab2313 <7723n12@sina.com>
2013-05-07 10:26:01 +0800

    fixed parseLink

--- TShot/Parse/Link.hs
+++ TShot/Parse/Link.hs
@@ -3,7 +3,7 @@ module TShot.Parse.Link where
 import Data.Char (toUpper)
 
 parseLink :: String -> String
-parseLink link = case takeWhile (== ':') link of
+parseLink link = case takeWhile (/= ':') link of
                    "magnet" -> parseMagnet link
                    _ -> if length link == 40 then map toUpper link
                                               else error "What's the link?"
