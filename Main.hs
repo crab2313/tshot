e4e393ef8215ee1a7189d71f70b43d00072553fc
crab2313 <crab2313@gmail.com>
2013-11-10 01:24:40 +0800

    Main.hs: checkProxy

--- Main.hs
+++ Main.hs
@@ -1,8 +1,12 @@
-module Main(main) where
+module Main (main) where
 
-import Data.List(intercalate)
-import System.Environment(getArgs)
+import Data.List (intercalate)
+import System.Environment (getArgs, getEnv)
+import Control.Exception 
+import System.IO.Error
+import Control.Monad (guard)
 
+import Network.HTTP.Proxy
 import Network.TShot.Parser
 import Network.TShot.Remote
 
@@ -15,3 +19,12 @@ main = do
 	    fname id name n = intercalate "-" [eName, show(id), show(n), ".tb"]
                 where eName = map (\c -> if c == '/' then '-' else c) name
                 
+-- checking whether we need a proxy connection
+checkProxy :: IO Proxy
+checkProxy = do
+    envProxy <- tryJust (guard . isDoesNotExistError) $ getEnv "http_proxy"
+    case envProxy of
+        Left _ -> return noProxy
+        Right proxy -> case parseProxy proxy of
+            Just p -> return p
+            Nothing -> return noProxy
